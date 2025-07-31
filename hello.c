2659486c3bceb2bdcfba7f5d4191d6b826dd9720
Caleb Rogers <caleb.rogers@gmail.com>
2025-07-31 14:04:26 -0700

    Test

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@
 
 int main(int argc, char *argv[]) {
   puts("Hello, world! Let's try another commit.");
-  puts("Hello, world! Let's try another commit.");
+  puts("Changing the words of the second line.");
   return EXIT_SUCCESS;
 }
